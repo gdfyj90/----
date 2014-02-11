244af81e907c0d5df3aecbface970864147369c3
fyj <fyj@promote.cache-dns.local>
2014-02-11 21:03:47 +0800

    Initial Commit

---
+++ RecordTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RecordTest
+//
+//  Created by fyj on 14-2-11.
+//  Copyright (c) 2014年 fyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
